fb6d31b7814175e7c3c7b868ef478eb34207688e
Gregory Sosa <gregory.sosa@gmail.com>
2022-10-13 00:31:07 -0400

    fix: fix write func removing de deadlock

--- alog.go
+++ alog.go
@@ -65,8 +65,7 @@ func (al Alog) formatMessage(msg string) string {
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	al.m.Lock()
-	defer al.m.Unlock()
+
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
 		go func(err error) {
